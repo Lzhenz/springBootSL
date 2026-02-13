893f1af0e517dd171f93d787226d8a59db14371b
Lzhenz <83808011+Lzhenz@users.noreply.github.com>
2026-02-13 02:42:22 +0000

    pcform init

--- src/main/java/com/example/helloSpringBoot/HelloSpringbootApplication.java
+++ src/main/java/com/example/helloSpringBoot/HelloSpringbootApplication.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.helloSpringBoot;
 
 import org.springframework.boot.SpringApplication;
@@ -9,3 +10,16 @@ public class HelloSpringbootApplication {
         SpringApplication.run(HelloSpringbootApplication.class, args);
     }
 }
+=======
+package com.example.helloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringbootApplication.class, args);
+    }
+}
+>>>>>>> 9131b08 (初始框架搭建02-01)
