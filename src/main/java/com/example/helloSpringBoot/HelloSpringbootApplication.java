e55ca8ab005706d3bc640eebd66891449acfcdc7
Lzhenz <2521071907@qq.com>
2026-02-14 12:19:43 +0900

    init on pc

--- src/main/java/com/example/helloSpringBoot/HelloSpringbootApplication.java
+++ src/main/java/com/example/helloSpringBoot/HelloSpringbootApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.helloSpringBoot;
 
 import org.springframework.boot.SpringApplication;
@@ -10,16 +9,3 @@ public class HelloSpringbootApplication {
         SpringApplication.run(HelloSpringbootApplication.class, args);
     }
 }
-=======
-package com.example.helloSpringBoot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HelloSpringbootApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloSpringbootApplication.class, args);
-    }
-}
->>>>>>> 9131b08 (初始框架搭建02-01)
